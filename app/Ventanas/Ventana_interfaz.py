fe734d5b913fed51de9e6930ea5adbf6d54f5e7b
kurovoxx <migueldbzns@gmail.com>
2024-08-30 11:22:42 -0400

    reordenado

--- app/Ventanas/Ventana_interfaz.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-import customtkinter as ctk
-
-
-class New_ventana(ABC):
-    def __init__(self, parent, color):
-        self.parent = parent
-        self.sub = ctk.CTkFrame(parent, fg_color=color, corner_radius=0)
-        self.sub.pack(fill='both', expand=True)
--- app/main.py
+++
@@ -1,5 +0,0 @@
-from Ventanas.Ventana_principal import Main
-
-app = Main()
-app.resizable(False, False)
-app.mainloop()
