01505d1da3233fe354b54ac9d935095aaf6628e4
sonickiller39 <sonickiller39@gmail.com>
2024-09-01 23:57:12 -0400

    Se eliminó lo relacionado a Canvas

--- Ventanas/Registro_Alimento.py
+++ Ventanas/Registro_Alimento.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from tkinter import Canvas, messagebox
+from tkinter import messagebox
 from Ventanas.Ventana_interfaz import New_ventana
 
 
@@ -9,11 +9,6 @@ class Registro_Alimento(New_ventana):
         self.add_widget_registro()
 
     def add_widget_registro(self):
-        # Rectángulo que acompaña el label "Agregar alimento"
-        self.canvas_agregar = Canvas(self.sub, bg="gray", highlightthickness=0)
-        self.canvas_agregar.place(relx=0.1, rely=0.3, relwidth=0.3, relheight=0.1)
-        self.canvas_agregar.create_rectangle(2, 2, 298, 98, outline="", fill="gray") 
-
         # Label "Agregar alimento"
         self.label_agregar = ctk.CTkLabel(self.sub, text="Agregar alimento", text_color="white", bg_color="black")
         self.label_agregar.place(relx=0.1, rely=0.3, relwidth=0.3, relheight=0.05)
@@ -32,11 +27,6 @@ class Registro_Alimento(New_ventana):
         # Evento para manejar la selección del combobox
         self.combo_box.bind("<<ComboboxSelected>>", self.predeterminado)
 
-        # Rectángulo que acompaña el Label "Buscador de alimentos"
-        self.canvas_buscar = Canvas(self.sub, bg="gray", highlightthickness=0)  
-        self.canvas_buscar.place(relx=0.1, rely=0.45, relwidth=0.3, relheight=0.1)
-        self.canvas_buscar.create_rectangle(2, 2, 298, 98, outline="", fill="gray")  
-
         # Label "Buscador de alimentos"
         self.label_buscar = ctk.CTkLabel(self.sub, text="Buscador de alimentos", text_color="white", bg_color="black")
         self.label_buscar.place(relx=0.1, rely=0.45, relwidth=0.3, relheight=0.05)
@@ -51,11 +41,7 @@ class Registro_Alimento(New_ventana):
                                            command=self.boton_agregar_click)
         self.boton_agregar.place(relx=0.1, rely=0.7, relwidth=0.3, relheight=0.05)
         
-        # Registro
-        self.canvas_registro = Canvas(self.sub, bg="#1f2329", highlightthickness=0)
-        self.canvas_registro.place(relx=0.5, rely=0.3, relwidth=0.3, relheight=0.1)
-        self.canvas_registro.create_rectangle(2, 2, 298, 98, outline="", fill="#1f2329") 
-        
+        # Registro        
         self.label_registro = ctk.CTkLabel(self.sub, text="Último alimento registrado: ", text_color="white", bg_color="#183549")
         self.label_registro.place(relx=0.5, rely=0.3, relwidth=0.3, relheight=0.05)
         
@@ -63,7 +49,6 @@ class Registro_Alimento(New_ventana):
         self.label_segundo_registro.place(relx=0.5, rely=0.35, relwidth=0.3, relheight=0.05)
 
     def predeterminado(self): # Esta función verifica si el mensaje predeterminado está seleccionado o no para limpiarlo al seleccionar una opción
-        
         if self.combo_box.get() == "Seleccionar alimento":
             self.combo_box.set("")  
 
