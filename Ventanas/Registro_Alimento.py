06da5585a78a51a8d6f72372ecc707d14b46580b
sonickiller39 <sonickiller39@gmail.com>
2024-09-01 22:36:48 -0400

    Actualización
    - Se modificaron algunos colores.
    - Se modificó el ctkmessagebox por messagebox.showinfo.
    - Se añadió una función de prueba, la cual hace que al escribir algo en el entry y darle al botón registrar, este aparecerá en el label de la derecha como último registro.

--- Ventanas/Registro_Alimento.py
+++ Ventanas/Registro_Alimento.py
@@ -1,7 +1,6 @@
 import customtkinter as ctk
-from tkinter import Canvas
+from tkinter import Canvas, messagebox
 from Ventanas.Ventana_interfaz import New_ventana
-from CTkMessagebox import CTkMessagebox
 
 
 class Registro_Alimento(New_ventana):
@@ -10,7 +9,7 @@ class Registro_Alimento(New_ventana):
         self.add_widget_registro()
 
     def add_widget_registro(self):
-        # Rectangulo que acompaña el label "Agregar alimento"
+        # Rectángulo que acompaña el label "Agregar alimento"
         self.canvas_agregar = Canvas(self.sub, bg="gray", highlightthickness=0)
         self.canvas_agregar.place(relx=0.1, rely=0.3, relwidth=0.3, relheight=0.1)
         self.canvas_agregar.create_rectangle(2, 2, 298, 98, outline="", fill="gray") 
@@ -25,7 +24,7 @@ class Registro_Alimento(New_ventana):
                                          button_hover_color="white", text_color="white")
         self.combo_box.place(relx=0.1, rely=0.35, relwidth=0.3, relheight=0.05)
         
-        # Rectangulo que acompaña el Label "Buscador de alimentos"
+        # Rectángulo que acompaña el Label "Buscador de alimentos"
         self.canvas_buscar = Canvas(self.sub, bg="gray", highlightthickness=0)  
         self.canvas_buscar.place(relx=0.1, rely=0.45, relwidth=0.3, relheight=0.1)
         self.canvas_buscar.create_rectangle(2, 2, 298, 98, outline="", fill="gray")  
@@ -35,7 +34,8 @@ class Registro_Alimento(New_ventana):
         self.label_buscar.place(relx=0.1, rely=0.45, relwidth=0.3, relheight=0.05)
         
         # Se crea el Entry "Buscar alimento"
-        self.entry_buscar = ctk.CTkEntry(self.sub, corner_radius=0, placeholder_text="Buscar alimento", placeholder_text_color="black", border_width=0, fg_color="white") 
+        self.entry_buscar = ctk.CTkEntry(self.sub, corner_radius=0, placeholder_text="Buscar alimento", placeholder_text_color="black", border_width=0, fg_color="white",
+                                         text_color="black") 
         self.entry_buscar.place(relx=0.1, rely=0.5, relwidth=0.3, relheight=0.1) 
 
         # Se crea el botón "Registrar"
@@ -43,16 +43,24 @@ class Registro_Alimento(New_ventana):
                                            command=self.boton_agregar_click)
         self.boton_agregar.place(relx=0.1, rely=0.7, relwidth=0.3, relheight=0.05)
         
-        
         # Registro
+        self.canvas_registro = Canvas(self.sub, bg="#1f2329", highlightthickness=0)
+        self.canvas_registro.place(relx=0.5, rely=0.3, relwidth=0.3, relheight=0.1)
+        self.canvas_registro.create_rectangle(2, 2, 298, 98, outline="", fill="#1f2329") 
         
-        self.canvas_agregar = Canvas(self.sub, bg="#1f2329", highlightthickness=0)
-        self.canvas_agregar.place(relx=0.5, rely=0.3, relwidth=0.3, relheight=0.1)
-        self.canvas_agregar.create_rectangle(2, 2, 298, 98, outline="", fill="#1f2329") 
+        self.label_registro = ctk.CTkLabel(self.sub, text="Último alimento registrado: ", text_color="white", bg_color="#183549")
+        self.label_registro.place(relx=0.5, rely=0.3, relwidth=0.3, relheight=0.05)
         
-        self.label_agregar = ctk.CTkLabel(self.sub, text="Último alimento registrado:" , text_color="white", bg_color="#183549")
-        self.label_agregar.place(relx=0.5, rely=0.3, relwidth=0.3, relheight=0.05)
+        self.label_segundo_registro = ctk.CTkLabel(self.sub, text="", text_color="white", bg_color="#1f2329")
+        self.label_segundo_registro.place(relx=0.5, rely=0.35, relwidth=0.3, relheight=0.05)
 
     def boton_agregar_click(self):  # Prueba de que funciona el botón "Registrar"
-        CTkMessagebox(title="Alimento registrado", message="Se registraron x calorias creo", icon="info") #Mensaje pro, funcionalidad basica
-        print("Botón 'Registrar' clickeado") 
+        alimento_registrado = self.entry_buscar.get()  # Obtiene el valor del Entry
+        self.actualizar_registro(alimento_registrado)
+        messagebox.showinfo("Alimento registrado", f"Se registró el alimento: {alimento_registrado}")
+        print(f"Botón 'Registrar' clickeado. Alimento registrado: {alimento_registrado}")
+    
+    def actualizar_registro(self, alimento):
+        # Actualiza el texto del label con el último alimento registrado
+        self.label_registro.configure(text=f"Último alimento registrado: {alimento}")
+        self.label_segundo_registro.configure(text=alimento)
