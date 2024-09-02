052ab0e887d6c6e113fec6896e229cfb99ddc322
sonickiller39 <sonickiller39@gmail.com>
2024-09-01 23:44:27 -0400

    Mensaje predeterminado combobox
    Se añadió una función para que el combobox tenga un mensaje predeterminado y este desaparezca al seleccionar una opción.

--- Ventanas/Registro_Alimento.py
+++ Ventanas/Registro_Alimento.py
@@ -19,11 +19,19 @@ class Registro_Alimento(New_ventana):
         self.label_agregar.place(relx=0.1, rely=0.3, relwidth=0.3, relheight=0.05)
 
         # ComboBox
-        self.combo_box = ctk.CTkComboBox(self.sub, corner_radius=0, fg_color="#183549", values=["Opción 1", "Opción 2", "Opción 3"],
+        self.alimentos = ["Alimento 1", "Alimento 2", "Alimento 3"]
+        self.combo_box = ctk.CTkComboBox(self.sub, corner_radius=0, fg_color="#183549",
+                                         values=self.alimentos,  
                                          border_width=0, button_color="#26656D",
                                          button_hover_color="white", text_color="white")
         self.combo_box.place(relx=0.1, rely=0.35, relwidth=0.3, relheight=0.05)
-        
+
+        # Mensaje "predeterminado" para el combobox
+        self.combo_box.set("Seleccionar alimento")  
+
+        # Evento para manejar la selección del combobox
+        self.combo_box.bind("<<ComboboxSelected>>", self.predeterminado)
+
         # Rectángulo que acompaña el Label "Buscador de alimentos"
         self.canvas_buscar = Canvas(self.sub, bg="gray", highlightthickness=0)  
         self.canvas_buscar.place(relx=0.1, rely=0.45, relwidth=0.3, relheight=0.1)
@@ -54,6 +62,10 @@ class Registro_Alimento(New_ventana):
         self.label_segundo_registro = ctk.CTkLabel(self.sub, text="", text_color="white", bg_color="#1f2329")
         self.label_segundo_registro.place(relx=0.5, rely=0.35, relwidth=0.3, relheight=0.05)
 
+    def predeterminado(self): # Esta función verifica si el mensaje predeterminado está seleccionado o no para limpiarlo al seleccionar una opción
+        
+        if self.combo_box.get() == "Seleccionar alimento":
+            self.combo_box.set("")  
 
     def boton_agregar_click(self):  # Prueba de que funciona el botón "Registrar"
         alimento_registrado = self.entry_buscar.get()  # Obtiene el valor del Entry
