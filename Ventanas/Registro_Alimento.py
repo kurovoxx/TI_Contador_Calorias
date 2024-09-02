f798d18488e7f2e489a991fa8688056971b2193c
sonickiller39 <sonickiller39@gmail.com>
2024-09-01 23:25:31 -0400

    Ingresar caloría como prueba
    Se modificó el segundo label de registro. Ahora registra las calorías del alimento como prueba, cosa que al añadir el alimento también se pida en consola la cantidad de calorías.

--- Ventanas/Registro_Alimento.py
+++ Ventanas/Registro_Alimento.py
@@ -34,8 +34,8 @@ class Registro_Alimento(New_ventana):
         self.label_buscar.place(relx=0.1, rely=0.45, relwidth=0.3, relheight=0.05)
         
         # Se crea el Entry "Buscar alimento"
-        self.entry_buscar = ctk.CTkEntry(self.sub, corner_radius=0, placeholder_text="Buscar alimento", placeholder_text_color="black", border_width=0, fg_color="white",
-                                         text_color="black") 
+        self.entry_buscar = ctk.CTkEntry(self.sub, corner_radius=0, placeholder_text="Buscar alimento", placeholder_text_color="black",
+                                         border_width=0, fg_color="white", text_color="black") 
         self.entry_buscar.place(relx=0.1, rely=0.5, relwidth=0.3, relheight=0.1) 
 
         # Se crea el botón "Registrar"
@@ -54,6 +54,7 @@ class Registro_Alimento(New_ventana):
         self.label_segundo_registro = ctk.CTkLabel(self.sub, text="", text_color="white", bg_color="#1f2329")
         self.label_segundo_registro.place(relx=0.5, rely=0.35, relwidth=0.3, relheight=0.05)
 
+
     def boton_agregar_click(self):  # Prueba de que funciona el botón "Registrar"
         alimento_registrado = self.entry_buscar.get()  # Obtiene el valor del Entry
         self.actualizar_registro(alimento_registrado)
@@ -61,6 +62,7 @@ class Registro_Alimento(New_ventana):
         print(f"Botón 'Registrar' clickeado. Alimento registrado: {alimento_registrado}")
     
     def actualizar_registro(self, alimento):
+        coso = int(input("Ingrese la cantidad de calorías: "))
         # Actualiza el texto del label con el último alimento registrado
         self.label_registro.configure(text=f"Último alimento registrado: {alimento}")
-        self.label_segundo_registro.configure(text=alimento)
+        self.label_segundo_registro.configure(text=f"Calorías totales: {coso} kcal")
