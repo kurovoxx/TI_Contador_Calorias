0a94dc05d1f598becbe962d7c565c779f8b92d91
nose <109933877+Halopro2020@users.noreply.github.com>
2024-09-10 15:17:25 -0300

    Update Agregar_Alimento.py

--- Ventanas/Agregar_Alimento.py
+++ Ventanas/Agregar_Alimento.py
@@ -19,18 +19,6 @@ class Agregar_Alimento(New_ventana):
         self.cursor = self.conn.cursor()
 
     def add_widget_agregar(self):
-
-        self.canvas = tk.Canvas(self.sub, width=800, height=600)
-        self.canvas.place(relx=0.5, rely=0.5, anchor="center")
-
-        # Mostrar imagen con PIL
-        self.img_alimento = Image.open("./img/a_alimento.png")
-        self.img_alimento = self.img_alimento.resize((800, 600), Image.Resampling.LANCZOS)
-        self.img_alimento_tk = ImageTk.PhotoImage(self.img_alimento)
-
-        # Muestra imagen en el canvas
-        self.canvas.create_image(0, 0, anchor="nw", image=self.img_alimento_tk)
-
         # Label "agregar" de alimentos
         self.label_agregar = ctk.CTkLabel(self.sub, font=("FuturaNo2", 20), text="Agregar Alimentos", text_color="white", bg_color="black")
         self.label_agregar.place(relx=0.1, rely=0.2, relwidth=0.3, relheight=0.05)
