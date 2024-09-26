75c534c9c491e0618b8468e969a0941cebd6ce24
sonickiller39 <sonickiller39@gmail.com>
2024-09-26 19:46:04 -0300

    Actualización Gráficos
    - Se cambiaron los colores.
    - Ahora cuando hay datos en el eje X el titulo "Fecha" desaparece y se muestran solo los datos.
    - Ahora en el eje Y solo se muestra el titulo (al igual que Eje X) en caso de no haber datos. Cuando ya hayan datos, estos aparecen y el titulo se mantiene.
    - Se movieron las etiquetas eje X (Fechas) de tal manera que coincidan con la barra/punto correspondiente (Aún esto se ve mal cuando solo hay 1 dato, solo funciona cuando hay 2 o más datos).

--- Ventanas/Grafico.py
+++ Ventanas/Grafico.py
@@ -12,33 +12,61 @@ class Grafico(New_ventana):
         self.add_widget_graficos()
 
     def add_widget_graficos(self):
+        # Crear figura
         fig = Figure(figsize=(10, 5), dpi=100, facecolor='#404B4C')
         fig.subplots_adjust(wspace=0.4)
-        ax1 = fig.add_subplot(121)  
-        ax2 = fig.add_subplot(122)  
-        
+        ax1 = fig.add_subplot(121)
         fecha, cantidad = self.datos_calorias()
-        ax1.set_facecolor('#404B4C')
-        ax1.bar(fecha, cantidad, color='blue', edgecolor='black')
-        ax1.grid(True, which='both', axis='y', linestyle='--', linewidth=0.7, color='black')
-        ax1.set_title('Gráfico calorías vs tiempo', color='white')
-        ax1.set_ylabel('Calorías', color='white')
-        ax1.set_xlabel('Fecha', color='white')
-        ax1.tick_params(axis='x', rotation=45, labelsize=8) 
-
+        
+        # Datos (BD)
+        ax1.set_facecolor(oscuro)
+        ax2 = fig.add_subplot(122)
         fecha2, peso = self.datos_peso()
-        ax2.set_facecolor('#404B4C')
-        ax2.plot(fecha2, peso, color='green', marker='o') 
-        ax2.grid(True, which='both', axis='y', linestyle='--', linewidth=0.7, color='black')
-        ax2.set_title('Gráfico peso vs tiempo', color='white')
-        ax2.set_ylabel('Peso (kg)', color='white')
-        ax2.set_xlabel('Fecha', color='white')
-        ax2.tick_params(axis='x', rotation=45, labelsize=8) 
-
+        ax2.set_facecolor(oscuro)
+        
+        # Configuración de los ejes
+        ax1.grid(True, which='both', axis='y', linestyle='--', linewidth=0.6, color='gray')
+        ax1.set_title('Calorías vs Tiempo', color='white', fontsize=12)
+        ax1.set_ylabel('Calorías', color='white', fontsize=10)
+        ax1.set_xlabel('Fecha', color='white', fontsize=10)
+        ax2.grid(True, which='both', axis='y', linestyle='--', linewidth=0.6, color='gray')
+        ax2.set_title('Peso vs Tiempo', color='white', fontsize=12)
+        ax2.set_ylabel('Peso (kg)', color='white', fontsize=10)
+        ax2.set_xlabel('Fecha', color='white', fontsize=10)
+        
+        
+        # Ocultar numeros en caso de no haber datos (Eje Y)
+        if len(cantidad) > 0:
+            bars = ax1.bar(fecha, cantidad, color=azul_mas_clarito, edgecolor='black', linewidth=1.5)
+            for bar in bars:
+                bar.set_linewidth(1.5)
+                bar.set_edgecolor('white')
+                bar.set_linestyle((0, (5, 1)))
+            ax1.set_yticks(ax1.get_yticks())
+        else:
+            ax1.set_yticks([])  
+        if len(peso) > 0:
+            ax2.plot(fecha2, peso, color=celeste_pero_oscuro, marker='o', markersize=6, markerfacecolor='white', linestyle='-', linewidth=2.5)
+        else:
+            ax2.set_yticks([])
+        
+        # Desplazamiento de etiquetas
+        x_locs_barras = [bar.get_x() + bar.get_width() / 2 for bar in bars] if len(cantidad) > 0 else []
+        desplazamiento_barras = 0.2
+        x_locs_adjusted_barras = [x + desplazamiento_barras for x in x_locs_barras]
+        ax1.set_xticks(x_locs_adjusted_barras)
+        ax1.set_xticklabels(fecha, rotation=45, ha='right', fontsize=8, color='white')
+        x_locs_lineas = range(len(fecha2))
+        desplazamiento_lineas = 0.1
+        x_locs_adjusted_lineas = [x + desplazamiento_lineas for x in x_locs_lineas]
+        ax2.set_xticks(x_locs_adjusted_lineas)
+        ax2.set_xticklabels(fecha2, rotation=45, ha='right', fontsize=8, color='white')
+        
+        # Canvas
         canvas = FigureCanvasTkAgg(fig, master=self.panel_principal)
         canvas.draw()
         widget_canvas = canvas.get_tk_widget()
-        widget_canvas.place(x=50, y=50, width=800, height=550)
+        widget_canvas.place(x=50, y=10, width=800, height=600)
 
     def datos_calorias(self):
         conn = sqlite3.connect(f"./users/{self.usuario}/alimentos.db")
