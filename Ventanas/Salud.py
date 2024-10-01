360b7a5fc58c3dbb86f49c535a9561ce93a2ceab
Mxtsi <maxisaezb@gmail.com>
2024-10-01 20:10:48 -0300

    avance cambio de valores imc-tbm

--- Ventanas/Salud.py
+++ Ventanas/Salud.py
@@ -3,16 +3,15 @@ from Ventanas.update_peso import Peso
 from util.colores import *
 import customtkinter as ctk
 import sqlite3
-from CTkMessagebox import CTkMessagebox  # Importamos la librería para la messagebox
+from CTkMessagebox import CTkMessagebox
 
 class Salud(New_ventana):
     def __init__(self, panel_principal, color):
         super().__init__(panel_principal, color)
-        self.mostrar_messagebox()  # Llamamos a la función que muestra el mensaje al abrir la pestaña
+        self.mostrar_messagebox()
         self.add_widget_salud()
         self.update_health_metrics()
 
-    # Función para mostrar la CTkMessagebox
     def mostrar_messagebox(self):
         mensaje = ("Bienvenido a la sección Salud. Aquí podrás actualizar tu peso, medir pulsaciones, "
                    "calcular tu IMC Y TMB, además de realizar un seguimiento de tu progreso diario. "
@@ -20,79 +19,79 @@ class Salud(New_ventana):
         CTkMessagebox(title="Sección Salud", message=mensaje, icon="info", option_1="OK")
 
     def add_widget_salud(self):
-         # Botón "Actualizar Peso"
-         self.btn_actualizar_peso = ctk.CTkButton(self.sub, text="Actualizar Peso", width=150, height=50, fg_color="#28242c", command=self.actualizar_peso)
-         self.btn_actualizar_peso.place(x=50, y=50)  # Colocar en la posición exacta
+        # Botón "Actualizar Peso"
+        self.btn_actualizar_peso = ctk.CTkButton(self.sub, text="Actualizar Peso", width=150, height=50, fg_color="#28242c", command=self.actualizar_peso)
+        self.btn_actualizar_peso.place(x=50, y=50)
 
-         # Botón "Medir pulsaciones"
-         self.btn_medir_pulsaciones = ctk.CTkButton(self.sub, text="Medir pulsaciones", width=150, height=50, fg_color="#28242c")
-         self.btn_medir_pulsaciones.place(x=50, y=150)
+        # Botón "Medir pulsaciones"
+        self.btn_medir_pulsaciones = ctk.CTkButton(self.sub, text="Medir pulsaciones", width=150, height=50, fg_color="#28242c")
+        self.btn_medir_pulsaciones.place(x=50, y=150)
 
-         self.label_imc = ctk.CTkLabel(self.sub, text="IMC:", fg_color="#28242c", text_color="white", font=("Arial", 15), width=100, height=50)
-         self.label_imc.configure(corner_radius=5)
-         self.label_imc.place(x=500, y=50)
+        self.label_imc = ctk.CTkLabel(self.sub, text="IMC:", fg_color="#28242c", text_color="white", font=("Arial", 15), width=100, height=50)
+        self.label_imc.configure(corner_radius=5)
+        self.label_imc.place(x=500, y=50)
 
-         self.result_imc = ctk.CTkLabel(self.sub, text="", fg_color="#28242c", text_color="white", font=("Arial", 15),width=100, height=50)
-         self.result_imc.place(x=610, y=50)
+        self.result_imc = ctk.CTkLabel(self.sub, text="", fg_color="#28242c", text_color="white", font=("Arial", 15), width=100, height=50)
+        self.result_imc.place(x=610, y=50)
 
-         self.label_tmb = ctk.CTkLabel(self.sub, text="TMB:", fg_color="#28242c", text_color="white", font=("Arial", 15), width=100, height=50)
-         self.label_tmb.configure(corner_radius=5)
-         self.label_tmb.place(x=500, y=150)
+        self.label_tmb = ctk.CTkLabel(self.sub, text="TMB:", fg_color="#28242c", text_color="white", font=("Arial", 15), width=100, height=50)
+        self.label_tmb.configure(corner_radius=5)
+        self.label_tmb.place(x=500, y=150)
         
-         self.result_tmb = ctk.CTkLabel(self.sub, text="", fg_color="#28242c", text_color="white", font=("Arial", 15),width=100, height=50)
-         self.result_tmb.place(x=610, y=150)
+        self.result_tmb = ctk.CTkLabel(self.sub, text="", fg_color="#28242c", text_color="white", font=("Arial", 15), width=100, height=50)
+        self.result_tmb.place(x=610, y=150)
 
-         # Crear los 8 botones redondeados debajo de la barra
-         self.botones = []
-         self.estado_botones = [False] * 8  # Lista para almacenar el estado de cada botón (False = gris, True = verde)
+        # Crear los 8 botones redondeados debajo de la barra
+        self.botones = []
+        self.estado_botones = [False] * 8
 
-         for i in range(8):
-             boton = ctk.CTkButton(self.sub, text="", width=50, height=50, corner_radius=20, fg_color="#28242c", 
-                                   command=lambda b=i: self.toggle_color(b))
-             boton.place(x=50 + i * 60, y=400)  # Posicionar los botones
-             self.botones.append(boton)
+        for i in range(8):
+            boton = ctk.CTkButton(self.sub, text="", width=50, height=50, corner_radius=20, fg_color="#28242c", 
+                                  command=lambda b=i: self.toggle_color(b))
+            boton.place(x=50 + i * 60, y=400)
+            self.botones.append(boton)
 
-         # Barra inferior
-         self.barra_inferior = ctk.CTkProgressBar(self.sub, width=550, height=40, fg_color="#417156", progress_color="#ade28a")
-         self.barra_inferior.place(x=20, y=350)
-         self.barra_inferior.set(0.7)  # Ajuste del progreso 
+        # Barra inferior
+        self.barra_inferior = ctk.CTkProgressBar(self.sub, width=550, height=40, fg_color="#417156", progress_color="#ade28a")
+        self.barra_inferior.place(x=20, y=350)
+        self.barra_inferior.set(0.7)
 
-         # Etiquetas de Meta de Calorías y Vasos de Agua
-         self.label_meta_calorias = ctk.CTkLabel(self.sub, text="Meta de Calorías x/Meta", fg_color=None, text_color="black", font=("Arial", 15))
-         self.label_meta_calorias.place(x=600, y=350)
+        # Etiquetas de Meta de Calorías y Vasos de Agua
+        self.label_meta_calorias = ctk.CTkLabel(self.sub, text="Meta de Calorías x/Meta", fg_color=None, text_color="black", font=("Arial", 15))
+        self.label_meta_calorias.place(x=600, y=350)
 
-         self.label_vasos_agua = ctk.CTkLabel(self.sub, text="Vasos de Agua: x", fg_color=None, text_color="black", font=("Arial", 15))
-         self.label_vasos_agua.place(x=600, y=420)
+        self.label_vasos_agua = ctk.CTkLabel(self.sub, text="Vasos de Agua: x", fg_color=None, text_color="black", font=("Arial", 15))
+        self.label_vasos_agua.place(x=600, y=420)
 
     def toggle_color(self, indice):
-         # Cambia el estado del botón (True = verde, False = gris)
-         if self.estado_botones[indice]:  # Si el botón está activo (verde)
-             self.botones[indice].configure(fg_color="#28242c")  # Cambiar a gris
-             self.estado_botones[indice] = False  # Cambiar estado a inactivo
-         else:  # Si el botón está inactivo (gris)
-             self.botones[indice].configure(fg_color="green")  # Cambiar a verde
-             self.estado_botones[indice] = True  # Cambiar estado a activo
+        if self.estado_botones[indice]:
+            self.botones[indice].configure(fg_color="#28242c")
+            self.estado_botones[indice] = False
+        else:
+            self.botones[indice].configure(fg_color="green")
+            self.estado_botones[indice] = True
     
     def actualizar_peso(self):
-        Peso(self.sub, self.usuario)
+        Peso(self.sub, self.usuario, callback=self.update_health_metrics)
 
-    def update_health_metrics(self):
-        imc = self.calcular_imc()
-        tmb = self.calcular_TMB()
-
-        if imc is not None:
-            self.result_imc.configure(text=f"{imc:.2f}")
-        else:
-            self.result_imc.configure(text="Error")
-
-        if tmb is not None:
-            self.result_tmb.configure(text=f"{tmb:.2f}")
-        else:
-            self.result_tmb.configure(text="Error")
-        
-        
-        self.sub.update()
-
+    def get_latest_weight(self):
+        try:
+            conn = sqlite3.connect(f"./users/{self.usuario}/alimentos.db")
+            cursor = conn.cursor()
+            
+            cursor.execute("SELECT peso FROM peso ORDER BY fecha DESC LIMIT 1")
+            result = cursor.fetchone()
+            
+            if result is None:
+                raise ValueError("No se encontró ningún registro de peso")
+            
+            return result[0]
+        except (sqlite3.Error, ValueError) as e:
+            print(f"Error al obtener el peso más reciente: {e}")
+            return None
+        finally:
+            if conn:
+                conn.close()
 
     def calcular_imc(self):
         try:
@@ -103,12 +102,12 @@ class Salud(New_ventana):
             resultado_estatura = cursor.fetchone()
             if resultado_estatura is None:
                 raise ValueError("No se encontró la estatura para el usuario")
-            estatura = resultado_estatura[0] / 100  # Convert to meters
+            estatura = resultado_estatura[0] / 100  # Convertir a metros
 
             cursor.execute("SELECT peso FROM peso ORDER BY fecha DESC LIMIT 1")
             resultado_peso = cursor.fetchone()
             if resultado_peso is None:
-                raise ValueError("No se encontró el peso para el usuario")
+                raise ValueError("No se encontró ningún registro de peso")
             peso = resultado_peso[0]
 
             imc = peso / (estatura ** 2)
@@ -131,12 +130,12 @@ class Salud(New_ventana):
             if result is None:
                 raise ValueError("No se encontraron datos del usuario")
             estatura, edad, genero = result
-            estatura = estatura / 100  # Convert to meters
+            estatura = estatura / 100  # Convertir a metros
 
             cursor.execute("SELECT peso FROM peso ORDER BY fecha DESC LIMIT 1")
             resultado_peso = cursor.fetchone()
             if resultado_peso is None:
-                raise ValueError("No se encontró el peso para el usuario")
+                raise ValueError("No se encontró ningún registro de peso")
             peso = resultado_peso[0]
 
             if genero.lower() in ["hombre", "masculino"]:
@@ -154,5 +153,19 @@ class Salud(New_ventana):
         finally:
             if conn:
                 conn.close()
-    
 
+    def update_health_metrics(self):
+        imc = self.calcular_imc()
+        tmb = self.calcular_TMB()
+
+        if imc is not None:
+            self.result_imc.configure(text=f"{imc:.2f}")
+        else:
+            self.result_imc.configure(text="Error")
+
+        if tmb is not None:
+            self.result_tmb.configure(text=f"{tmb:.2f}")
+        else:
+            self.result_tmb.configure(text="Error")
+        
+        self.sub.update()
