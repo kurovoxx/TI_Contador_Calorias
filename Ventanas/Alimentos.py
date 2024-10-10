51bb5336a0618f3dbd80c30ed9079d0c1fefe9a2
Mxtsi <maxisaezb@gmail.com>
2024-10-10 19:01:27 -0300

    quite una con

--- Ventanas/Alimentos.py
+++ Ventanas/Alimentos.py
@@ -12,20 +12,9 @@ class Alimentos(New_ventana):
         self.widget_alimentos()
         self.mensage("Esta es la pestaña Admin alimentos, aqui podras ver todos los alimentos que has registrado, al igual que podras gestionar las calorias que tienen", "Admin Alimentos")
 
-    def mostrar_advertencia(self):
-        CTkMessagebox(title="Admin Alimentos", message="Esta es la pestaña Admin alimentos, aqui podras ver todos los alimentos que has registrado, al igual que podras gestionar las calorias que tienen", icon='info', option_1="Ok"),
-
-
     def widget_alimentos(self):
         self.treeview_alimentos = ctk.CTkFrame(self.sub, width=500)
         self.treeview_alimentos.pack(pady=60, padx=20, anchor="center")
-        self.boton_ayuda = ctk.CTkButton(self.sub, text="i",
-                                         command=self.mostrar_advertencia,
-                                         corner_radius=15,
-                                         width=30, height=30,
-                                         font=("Times New Roman", 25, "italic"),
-                                         text_color="white")
-        self.boton_ayuda.place(relx=0.97, rely=0.04, anchor="ne")
         
         self.label_food = ctk.CTkLabel(self.treeview_alimentos, text="Alimentos")
         self.label_food.pack(anchor="center")
