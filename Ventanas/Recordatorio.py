eade6797ee9b42b929f8788638ec723223638563
Hector Lepio <hlepio2024@alu.uct.cl>
2024-11-07 20:07:07 -0300

    hola

--- Ventanas/Recordatorio.py
+++ Ventanas/Recordatorio.py
@@ -13,13 +13,14 @@ class Recordatorio:
             message="No has registrado tu peso según la frecuencia establecida. Por favor, actualiza tu peso.",
             icon="warning", option_1="OK"
         )
+
     def mostrar_mensaje_recordatorio_unavez(self, cursor):
         fecha_hoy = datetime.now().date()
 
         if self.ultimo_msj != fecha_hoy:
             self.mostrar_mensaje_recordatorio()
             self.ultimo_msj = fecha_hoy
-        
+
             cursor.execute("""
                 UPDATE datos 
                 SET recordatorio = 'mostrado_hoy' 
@@ -27,74 +28,68 @@ class Recordatorio:
             """, (self.usuario,))
 
     def recordar_actualizar_peso(self):
-            try:
-                conn = sqlite3.connect(f"./users/{self.usuario}/alimentos.db")
-                cursor = conn.cursor()
-
-                cursor.execute("SELECT recordatorio, cantidad_dias FROM datos WHERE nombre = ?", (self.usuario,))
-                config = cursor.fetchone()
-
-                if config:
-                    estado, frecuencia = config
-                    frecuencia_dias = int(frecuencia.split()[0])
-
-                    cursor.execute("SELECT fecha FROM peso ORDER BY fecha DESC LIMIT 1")
-                    ultimo_registro = cursor.fetchone()
-
-                    if ultimo_registro and ultimo_registro[0]:
-                        ultima_fecha = datetime.strptime(ultimo_registro[0], '%d-%m-%Y')
-                        dias_diferencia = (datetime.now() - ultima_fecha).days
-
-                        if (dias_diferencia >= frecuencia_dias and estado != 'mostrado_hoy') or estado == 'mostrado':
-                            self.mostrar_mensaje_recordatorio_unavez(cursor)
-                        else:
-                            cursor.execute("""
-                                UPDATE datos 
-                                SET recordatorio = 'on' 
-                                WHERE nombre = ?
-                            """, (self.usuario,))
-                    else:
-                    
-                        if estado != 'mostrado_hoy':  
-                            self.mostrar_mensaje_recordatorio_unavez(cursor)
-
-                conn.commit()
-                conn.close()
-
-            except sqlite3.Error as e:
-                CTkMessagebox(title="Error", message=f"Error al acceder a la base de datos: {e}", icon="info", option_1="OK")
-            except Exception as e:
-                CTkMessagebox(title="Error", message=f"Error inesperado: {e}", icon="info", option_1="OK")
-                
-    #Funcion para que el recordatorio este por defecto en 1dia            
-    def recordatorio_por_defecto(usuario):
-<<<<<<< HEAD
-<<<<<<< Updated upstream
         try:
-=======
->>>>>>> ebdb8c442b5cf530a02a2eeaf613f48951de12c7
-            conn = sqlite3.connect(f"./users/{usuario}/alimentos.db")
+            conn = sqlite3.connect(f"./users/{self.usuario}/alimentos.db")
             cursor = conn.cursor()
-=======
-        conn = sqlite3.connect(f"./users/{usuario}/alimentos.db")
-        cursor = conn.cursor()
->>>>>>> Stashed changes
 
-        cursor.execute("SELECT cantidad_dias FROM datos WHERE nombre = ?", (usuario,))
-        resultado = cursor.fetchone()
+            cursor.execute("SELECT recordatorio, cantidad_dias FROM datos WHERE nombre = ?", (self.usuario,))
+            config = cursor.fetchone()
 
-        if resultado:
-            cursor.execute("""
-                UPDATE datos
-                SET cantidad_dias = '1 día'
-                WHERE nombre = ? AND (cantidad_dias IS NULL OR cantidad_dias = '')
-            """, (usuario,))
-        else:
-            cursor.execute("""
-                INSERT INTO datos (nombre, recordatorio, cantidad_dias)
-                VALUES (?, 'off', '1 día')
-            """, (usuario,))
+            if config:
+                estado, frecuencia = config
+                frecuencia_dias = int(frecuencia.split()[0])
 
-        conn.commit()
-        conn.close()
+                cursor.execute("SELECT fecha FROM peso ORDER BY fecha DESC LIMIT 1")
+                ultimo_registro = cursor.fetchone()
 
+                if ultimo_registro and ultimo_registro[0]:
+                    ultima_fecha = datetime.strptime(ultimo_registro[0], '%d-%m-%Y')
+                    dias_diferencia = (datetime.now() - ultima_fecha).days
+
+                    if (dias_diferencia >= frecuencia_dias and estado != 'mostrado_hoy') or estado == 'mostrado':
+                        self.mostrar_mensaje_recordatorio_unavez(cursor)
+                    else:
+                        cursor.execute("""
+                            UPDATE datos 
+                            SET recordatorio = 'on' 
+                            WHERE nombre = ?
+                        """, (self.usuario,))
+                else:
+                    if estado != 'mostrado_hoy':  
+                        self.mostrar_mensaje_recordatorio_unavez(cursor)
+
+            conn.commit()
+
+        except sqlite3.Error as e:
+            CTkMessagebox(title="Error", message=f"Error al acceder a la base de datos: {e}", icon="info", option_1="OK")
+        except Exception as e:
+            CTkMessagebox(title="Error", message=f"Error inesperado: {e}", icon="info", option_1="OK")
+        finally:
+            conn.close()
+
+    def recordatorio_por_defecto(self):
+        try:
+            conn = sqlite3.connect(f"./users/{self.usuario}/alimentos.db")
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT cantidad_dias FROM datos WHERE nombre = ?", (self.usuario,))
+            resultado = cursor.fetchone()
+
+            if resultado:
+                cursor.execute("""
+                    UPDATE datos
+                    SET cantidad_dias = '1 día'
+                    WHERE nombre = ? AND (cantidad_dias IS NULL OR cantidad_dias = '')
+                """, (self.usuario,))
+            else:
+                cursor.execute("""
+                    INSERT INTO datos (nombre, recordatorio, cantidad_dias)
+                    VALUES (?, 'off', '1 día')
+                """, (self.usuario,))
+
+            conn.commit()
+
+        except sqlite3.Error as e:
+            CTkMessagebox(title="Error", message=f"Error al acceder a la base de datos: {e}", icon="info", option_1="OK")
+        finally:
+            conn.close()
